4be472f1cf43ea5b834cc22c290d600b801af409
gshahbazian <gabeshahbazian@gmail.com>
2025-09-27 12:25:39 -0700

    under rgbs are jank

--- keyboards/crkbd/rev1/keymaps/gshahbazian/rgb_layers.c
+++ keyboards/crkbd/rev1/keymaps/gshahbazian/rgb_layers.c
@@ -7,8 +7,6 @@ static const rgb_t layer_colors[] = {
     [_NAV]     = {0, 0, 64},
     [_ADJUST]  = {64, 0, 0},
 };
-static const rgb_t underglow_color = {48, 48, 48};
-
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t highest_layer = get_highest_layer(layer_state | default_layer_state);
     if (highest_layer >= ARRAY_SIZE(layer_colors)) {
@@ -18,10 +16,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     rgb_t color = layer_colors[highest_layer];
 
     for (uint8_t index = led_min; index < led_max; ++index) {
-        if (g_led_config.flags[index] & LED_FLAG_UNDERGLOW) {
-            rgb_matrix_set_color(index, underglow_color.r, underglow_color.g, underglow_color.b);
-            continue;
-        }
         if (g_led_config.flags[index] & (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER)) {
             rgb_matrix_set_color(index, color.r, color.g, color.b);
         }
