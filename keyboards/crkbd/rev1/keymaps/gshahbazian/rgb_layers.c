cdd76ba25c05b43cc56ba676d1dfe33d144f4384
gshahbazian <gabeshahbazian@gmail.com>
2025-09-26 19:37:31 -0700

    color underglow

--- keyboards/crkbd/rev1/keymaps/gshahbazian/rgb_layers.c
+++ keyboards/crkbd/rev1/keymaps/gshahbazian/rgb_layers.c
@@ -14,6 +14,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         [_NAV]     = {0, 0, 64},     // blue
         [_ADJUST]  = {64, 0, 0},     // red
     };
+    static const layer_rgb_t underglow_color = {48, 48, 48};
 
     uint8_t highest_layer = get_highest_layer(layer_state | default_layer_state);
     if (highest_layer >= ARRAY_SIZE(layer_colors)) {
@@ -23,10 +24,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     layer_rgb_t color = layer_colors[highest_layer];
 
     for (uint8_t index = led_min; index < led_max; ++index) {
-        if (!(g_led_config.flags[index] & LED_FLAG_KEYLIGHT)) {
+        if (g_led_config.flags[index] & LED_FLAG_UNDERGLOW) {
+            rgb_matrix_set_color(index, underglow_color.r, underglow_color.g, underglow_color.b);
             continue;
         }
-        rgb_matrix_set_color(index, color.r, color.g, color.b);
+        if (g_led_config.flags[index] & LED_FLAG_KEYLIGHT) {
+            rgb_matrix_set_color(index, color.r, color.g, color.b);
+        }
     }
 
     return false;
